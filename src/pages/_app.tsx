d11a3f340870cfa6c6039277e52c2f2d1a445c36
j206 <35281996+j206@users.noreply.github.com>
2022-10-29 15:54:32 -0700

    Build out UI structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import 'tailwindcss/tailwind.css'
+import '../styles/global.css'
 import { SessionProvider } from "next-auth/react";
 
 
