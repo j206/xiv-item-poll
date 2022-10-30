4a21b41ec4e29f2835a1b88f850fda428699223b
j206 <35281996+j206@users.noreply.github.com>
2022-10-30 04:33:34 -0700

    Fix TRPC, hopefully

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import { type NextPage } from "next";
+import React from "react";
+import { getOptionsForVote } from "../utils/getRandomItem";
+import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
+  const [first, second] = React.useMemo(() => getOptionsForVote(), []);
+  const hello = trpc.hello.useQuery({ text: "client" });
+  if (!hello.data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <div className="text-center text-2xl">Which item is cooler?</div>
+      <div className="text-center text-2xl">
+        Which Ultimate Weapon is cooler?
+      </div>
       <div className="p-2" />
       <div className="flex max-w-2xl items-center justify-between rounded border p-8">
-        <div className="h-16 w-16 bg-red-200"></div>
+        <div className="h-16 w-16 bg-red-800">{hello.data.greeting}</div>
         <div className="p-8">Vs.</div>
-        <div className="h-16 w-16 bg-red-200"></div>
+        <div className="h-16 w-16 bg-red-800">wabba</div>
       </div>
     </div>
   );
--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,17 @@
-import { router } from "../trpc";
-import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
-
+import { z } from "zod";
+import { publicProcedure, router } from "../trpc";
 export const appRouter = router({
-  example: exampleRouter,
-  auth: authRouter,
+  hello: publicProcedure
+    .input(
+      z.object({
+        text: z.string().nullish(),
+      })
+    )
+    .query(({ input }) => {
+      return {
+        greeting: `hello ${input?.text ?? "world"}`,
+      };
+    }),
 });
-
 // export type definition of API
 export type AppRouter = typeof appRouter;
