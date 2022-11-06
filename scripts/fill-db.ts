206380d3969a9fcfc6eaf662d60b23e72254e434
j206 <35281996+j206@users.noreply.github.com>
2022-11-06 01:17:13 -0700

    Complete working DB backfill script

--- scripts/fill-db.ts
+++ scripts/fill-db.ts
@@ -1,14 +1,27 @@
-import { prisma } from "../src/server/db/client";
+import Prisma1, * as Prisma2 from "@prisma/client";
+import fetch from "node-fetch";
+
+const Prisma = Prisma1 || Prisma2;
+export const prisma = new Prisma.PrismaClient();
 
 const doBackfill = async () => {
   const ucob = await queryAPI(20959, 20974);
-  // const uwu = await queryAPI(22868, 22883);
-  // const tea = await queryAPI(28289, 28306);
-  // const dsr = await queryAPI(36943, 36962);
+  const uwu = await queryAPI(22868, 22883);
+  const tea = await queryAPI(28289, 28306);
+  const dsr = await queryAPI(36943, 36962);
 
-  // TODO: merge? for now just do ucob
-  const allWeapons = ucob;
-  const formattedWeapons = allWeapons.results.map(
+  // TODO: merge? for now just do them individually
+  addWeaponsToDb(ucob.Results);
+  addWeaponsToDb(uwu.Results);
+  addWeaponsToDb(tea.Results);
+  addWeaponsToDb(dsr.Results);
+  
+
+};
+
+const addWeaponsToDb = async (weaponSet: { ID: number; Name: string; ClassJobCategory: { Name: string; }; IconHD: string; }[]) => {
+  console.log(`Formatting data set...`)
+  const formattedWeapons = weaponSet.map(
     (w: {
       ID: number;
       Name: string;
@@ -18,10 +31,11 @@ const doBackfill = async () => {
       id: w.ID,
       name: w.Name,
       job: w.ClassJobCategory.Name,
-      iconUrl: `https://xivapi.com/item${w.IconHD}`,
+      iconUrl: `https://xivapi.com${w.IconHD}`,
     })
   );
-
+  
+  console.log(`Attempting to add to DB...`)
   const creation = await prisma.weapon.createMany({
     data: formattedWeapons,
   });
@@ -30,8 +44,9 @@ const doBackfill = async () => {
 };
 
 const queryAPI = async (first: number, last: number) => {
+  console.log(`Querying API for range: ${first}-${last}`)
   const response = await fetch(
-    `https://xivapi.com/search?indexes=Item&filters=ID%3E=${first},ID%3C=${last}&columns=ID,Name,IconHD,ClassJobCategory.Name`,
+    `https://xivapi.com/search?indexes=Item&filters=ID%3E=${first},ID%3C=${last}&columns=ID,Name,IconHD,ClassJobCategory.Name`
   ).then((response) => response.json());
 
   return response;
@@ -43,5 +58,3 @@ doBackfill();
 // UWU: https://xivapi.com/search?indexes=Item&filters=ID%3E=22868,ID%3C=22883&columns=ID,Name,IconHD,ClassJobCategory.Name
 // TEA: https://xivapi.com/search?indexes=Item&filters=ID%3E=28289,ID%3C=28306&columns=ID,Name,IconHD,ClassJobCategory.Name
 // DSR: https://xivapi.com/search?indexes=Item&filters=ID%3E=36943,ID%3C=36962&columns=ID,Name,IconHD,ClassJobCategory.Name
-
-//
